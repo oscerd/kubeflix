b7153741fd59fb68800e596b46a2b577e372895e
Ioannis Canellos <iocanel@gmail.com>
2016-03-09 12:34:59 +0200

    Make loadbroker more realistic. Make loanbroker apperar better on hystrix dashboard.

--- examples/loanbroker/broker/src/main/java/io/fabric8/kubeflix/examples/loanbroker/broker/RequestQuoteFromBankCommand.java
+++ examples/loanbroker/broker/src/main/java/io/fabric8/kubeflix/examples/loanbroker/broker/RequestQuoteFromBankCommand.java
@@ -18,6 +18,7 @@ package io.fabric8.kubeflix.examples.loanbroker.broker;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixCommandProperties;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceSpec;
@@ -26,27 +27,31 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-public class RequestQuoteCommand extends HystrixCommand<Quote> {
+public class RequestQuoteFromBankCommand extends HystrixCommand<Quote> {
 
     private final ClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
     private final RestTemplate template = new RestTemplate(httpRequestFactory);
 
     private final Service service;
     private final Long ssn;
+    private final Double amount;
     private final Integer duration;
 
-    protected RequestQuoteCommand(Service service, Long ssn, Integer duration) {
-        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(service.getMetadata().getName()))
+    protected RequestQuoteFromBankCommand(Service service, Long ssn, Double amount, Integer duration) {
+        super(Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(service.getMetadata().getName()))
+                .andCommandKey(HystrixCommandKey.Factory.asKey("RequestQuoteFromBank"))
                 .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
-                        .withExecutionTimeoutInMilliseconds(5000)));
+                        .withExecutionTimeoutInMilliseconds(10000)));
         this.service = service;
         this.ssn = ssn;
+        this.amount = amount;
         this.duration = duration;
     }
 
     @Override
     protected Quote run() throws Exception {
-        String url = URLUtils.join(getServiceURL(service), "/quote?ssn=" + ssn + "&duration=" + duration);
+        String url = URLUtils.join(getServiceURL(service), "/quote?ssn=" + ssn + "&duration=" + duration+"&amount="+amount);
         return template.getForEntity(url, Quote.class).getBody();
     }
 
