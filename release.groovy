73cc9b6583638170a6865645c05c161cd7fd859e
rawlingsj <rawlingsj80@gmail.com>
2016-04-12 11:24:47 +0100

    update pipeline to work with latest fabric8 release scripts

---
+++ release.groovy
@@ -0,0 +1,55 @@
+#!/usr/bin/groovy
+def updateDependencies(source){
+
+  def properties = []
+  properties << ['<fabric8.version>','io/fabric8/kubernetes-api']
+  properties << ['<docker.maven.plugin.version>','io/fabric8/docker-maven-plugin']
+
+  updatePropertyVersion{
+    updates = properties
+    repository = source
+    project = 'fabric8io/kubeflix'
+  }
+}
+
+def stage(){
+  return stageProject{
+    project = 'fabric8io/kubeflix'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.kubeflix'
+    githubOrganisation = 'fabric8io'
+    artifactIdToWatchInCentral = 'turbine-server'
+    artifactExtensionToWatchInCentral = 'war'
+    promoteToDockerRegistry = 'docker.io'
+    dockerOrganisation = 'fabric8'
+    imagesToPromoteToDockerHub = ['hystrix-dashboard','turbine-server']
+    extraImagesToTag = null
+  }
+}
+
+def mergePullRequest(prId){
+  mergeAndWaitForPullRequest{
+    project = 'fabric8io/kubeflix'
+    pullRequestId = prId
+  }
+
+}
+return this;
