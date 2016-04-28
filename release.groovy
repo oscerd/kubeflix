965db7d31778a35da430774ac1eb91afb0b4d4a1
Ioannis Canellos <iocanel@gmail.com>
2016-04-29 00:59:19 +0300

    Upgrade to latest spring-cloud-kubernetes version. Fix minor issue with the spring-cloud-kubernetes artifact in release.groovy.

--- release.groovy
+++ release.groovy
@@ -4,7 +4,7 @@ def updateDependencies(source){
   def properties = []
   properties << ['<fabric8.version>','io/fabric8/kubernetes-generator']
   properties << ['<kubernetes-client.version>','io/fabric8/kubernetes-client']
-  properties << ['<spring-cloud-kubernetes.version>','io/fabric8/spring-cloud-kubernetes']
+  properties << ['<spring-cloud-kubernetes.version>','io/fabric8/spring-cloud-kubernetes-core']
   properties << ['<docker.maven.plugin.version>','io/fabric8/docker-maven-plugin']
   properties << ['<fabric8-maven-plugin.version>','io/fabric8/fabric8-maven-plugin']
 
