7019470f06d1e2e75ca0cc74290f367d662ef3a4
James Strachan <james.strachan@gmail.com>
2016-10-25 10:02:44 +0100

    lets avoid updating versions due to FIS releases breaking things

--- release.groovy
+++ release.groovy
@@ -2,11 +2,11 @@
 def updateDependencies(source){
 
   def properties = []
-  properties << ['<fabric8.version>','io/fabric8/kubernetes-generator']
-  // TODO - tends to update to 1.2.0.redhat-133!!
+  // lets disable property update as FIS releases break it!
+  //properties << ['<fabric8.version>','io/fabric8/kubernetes-generator']
   //properties << ['<fabric8.maven.plugin.version>','io/fabric8/fabric8-maven-plugin']
-  properties << ['<kubernetes-client.version>','io/fabric8/kubernetes-client']
-  properties << ['<spring-cloud-kubernetes.version>','io/fabric8/spring-cloud-kubernetes-core']
+  //properties << ['<kubernetes-client.version>','io/fabric8/kubernetes-client']
+  //properties << ['<spring-cloud-kubernetes.version>','io/fabric8/spring-cloud-kubernetes-core']
 
   updatePropertyVersion{
     updates = properties
